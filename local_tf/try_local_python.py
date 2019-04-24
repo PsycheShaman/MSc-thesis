caca6a3ad116d7e7349e908d8dd2538ab50084d3
PsycheShaman <gerhard@DESKTOP-FJESODP.22seven.local>
2019-04-24 23:02:08 +0200

    Local tensorflow works on Python, not yet R

---
+++ local_tf/try_local_python.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import numpy
+import tensorflow as tf
+
+
+with tf.Session() as sess:
+  devices = sess.list_devices()
+  
+print(devices)
+
+with tf.device('/gpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b)
+
+with tf.Session() as sess:
+    print (sess.run(c))
+    
+ #   'C:\Users\gerhard\AppData\Roaming\Python\Python35\Scripts'
+ 
+ tf.test.is_gpu_available(
+    cuda_only=True,
+    min_cuda_compute_capability=None
+)
--- local_tf/try_setup_local.R
+++ local_tf/try_setup_local.R
@@ -2,14 +2,19 @@ install.packages("keras")
 
 require(keras)
 
-#install_keras(tensorflow = "gpu")
-
 require(reticulate)
-
 reticulate::use_condaenv("py35",required = T)
 
-install_keras(tensorflow="gpu")
 
-file.edit('~/.Renviron')
 
-require(keras)
+install_keras(tensorflow = "gpu")
+
+# 
+# 
+#
+# 
+# install_keras(tensorflow="gpu")
+# 
+# file.edit('~/.Renviron')
+# 
+# require(keras)
