7f8eb0cbae4c7f5181c8666c81e988cf7fc2b4ce
PsycheShaman <gerhard@DESKTOP-HISBRIN.22seven.local>
2019-07-18 22:32:20 +0200

    Some work on thesis

--- NEW/data_preprocessing/untitled0.py
+++ NEW/data_preprocessing/untitled0.py
@@ -8,14 +8,21 @@ Created on Sat Jul 13 17:06:25 2019
 from ast import literal_eval
 import json
 
-d = open("C:/Users/gerhard/Documents/msc-thesis-data/hijing-sim/pythonDict.txt")
-d = d.read()
-#d = d + "}"
-d = literal_eval(d)
-jayson = json.dumps(d,indent=4,sort_keys=True)
-name1="C:\\Users\\gerhard\\Documents\\msc-thesis-data\\hijing-sim\\" + str(1)
-name2=".json"
-name=name1+name2
-outfile = open(name,"w")
-outfile.write(jayson)
-print(name)
+import glob
+
+files = glob.glob("C:\\Users\\gerhard\\Documents\\msc-thesis-data\\hijing-sim/**/pythonDict.txt", recursive=True)
+ 
+j=0   
+for i in files[120:]:
+    j=j+1
+    d = open(i)
+    d = d.read()
+    #d = d + "}"
+    d = literal_eval(d)
+    jayson = json.dumps(d,indent=4,sort_keys=True)
+    name1="C:\\Users\\gerhard\\Documents\\msc-thesis-data\\hijing-sim\\" + str(j)
+    name2=".json"
+    name=name1+name2
+    outfile = open(name,"w")
+    outfile.write(jayson)
+    print(name)
