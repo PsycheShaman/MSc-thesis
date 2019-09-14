18ccfe3504018d92381d347eb4f33372ca41f456
PsycheShaman <christiaan.viljoen@cern.ch>
2019-09-14 14:49:05 +0200

    Cutting out the unnecessary, moving on

--- NEW/ML/real-vs-aae/real-v-aae.py
+++ NEW/ML/real-vs-aae/real-v-aae.py
@@ -5,3 +5,39 @@ Created on Sat Sep 14 13:39:15 2019
 @author: gerhard
 """
 
+import glob
+
+import numpy as np
+
+import pickle
+
+def load_real_data():
+        x_files = glob.glob("C:\\Users\\gerhard\\Documents\\msc-thesis-data\\cnn\\x_*.pkl")
+        
+        with open(x_files[0],'rb') as x_file:
+            x = pickle.load(x_file)
+        
+        for i in x_files[1:5]:
+            with open(i,'rb') as x_file:
+                xi = pickle.load(x_file)
+                x = np.concatenate((x,xi),axis=0)
+
+
+def load_simulated_data():
+    x_files = glob.glob("C:\\Users\\gerhard\\Documents\\Keras-GAN\\newAAE\\v11\\simulated_data\\*.npy")
+    
+    with open(x_files[0],'rb') as x_file:
+            x = np.load(x_file)
+    
+    for i in x_files[1:]:
+        with open(i,'rb') as x_file:
+            xi = np.load(x_file)
+            x = np.concatenate((x,xi),axis=0)
+            print(x.shape)
+
+sim = load_simulated_data()
+
+
+
+
+
